590004d6c2a2e06520c9ccd9fb7cb13e4ab818db
MrT3313 <rturgeon13@gmail.com>
2019-06-29 12:16:44 -0400

    dbConfig.js & first FAILING test

---
+++ api/models/gamesModel.js
@@ -0,0 +1,13 @@
+const KNEX_DB = require('../../data/dbConfig.js')
+
+module.exports = {
+    insert,
+    getAll,
+}
+
+async function insert(game) {
+    return null
+}
+async function getAll() {
+    return null
+}
---
+++ api/models/gamesModel.test.js
@@ -0,0 +1,26 @@
+const KNEX_DB = require('../../data/dbConfig.js')
+
+const gamesModel = require('./gamesModel.js')
+
+describe('the games model', () => {
+    // INSERT
+    afterEach( async () => {
+        await KNEX_DB('GAMES'),truncate()
+    })
+
+    it('should insert new game into DB', async () => {
+        // using model method
+            await gamesModel.insert({
+                title: 'Watching Paint Dry: The Sequel',
+                genre: 'Exhilarating Action RPG',
+                releaseYear: 2077
+            })
+
+        // confirm with KNEX
+            const games = await KNEX_DB('GAMES')
+
+            expect(games).toHaveLength(1)
+            expect(games[0].title).toBe('Watching Paint Dry: The Sequel')
+            // expect(res.status).toBe(201)
+    })
+})
